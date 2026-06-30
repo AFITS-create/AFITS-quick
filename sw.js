08cdb9fe4ffd855eded8d49486b0b8db306bd3b4
AFITS-create <askforitsolution@gmail.com>
2026-06-30 22:17:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // AFITS Quick � Service Worker
 // Provides offline caching for a faster, app-like experience
 
-const CACHE_NAME = 'afits-quick-v54';
+const CACHE_NAME = 'afits-quick-v56';
 
 try {
   importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
